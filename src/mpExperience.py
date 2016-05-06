a6dd6d64a415dcc2ae6dab22480a0e53e7009652
Quentin De Coninck <quentin.deconinck@uclouvain.be>
2016-05-06 10:19:03 +0200

    Revert "mpExperience: with router view"
    This trace does not reveal anything useful...
    
    This reverts commit fddccd02dd3fee38817d45e339009fd207ba44f0.

--- src/mpExperience.py
+++ src/mpExperience.py
@@ -1,5 +1,4 @@
 from mpParamXp import MpParamXp
-from mpTopo import MpTopo
 
 class MpExperience:
 	PING = "ping"
@@ -189,8 +188,5 @@ class MpExperience:
 		if spcap == "yes" :
 			self.mpTopo.commandTo(self.mpConfig.server,
 					"tcpdump -i any -s " + snaplenpcap + " -w server.pcap &")
-		if spcap == "yes" and cpcap == "yes":
-			router = self.mpTopo.getHost(MpTopo.routerName)
-			self.mpTopo.commandTo(router, "tcpdump -i any -s " + snaplenpcap + " -w router.pcap &")
 		if spcap == "yes" or cpcap == "yes":
 			self.mpTopo.commandTo(self.mpConfig.client,"sleep 5")
